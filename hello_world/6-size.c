b55ded710b64f8c04471b47bcc5b83ae095769f4
bumble-beed <shirleen.workstuff@gmail.com>
2026-02-11 08:10:36 +0000

    add new file 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print size of various types
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Char size: %lu byte(s)\n", sizeof(char));
+	printf("Int size: %lu byte(s)\n", sizeof(int));
+	return (0);
+}
