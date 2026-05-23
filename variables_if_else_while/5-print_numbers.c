46efffe1c5ef19adb92e0751628cd01485fd005b
bumble-beed <shirleen.workstuff@gmail.com>
2026-05-23 07:44:49 +0000

    print single digit numbers

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -2,16 +2,15 @@
 
 /**
  * main - prints single digit numbers of base 10
- * 
+ *
  * Return: 0
  */
 int main(void)
 {
 	int n;
 
-	for (n = 0; n <= 9; n++) {
+	for (n = 0; n <= 9; n++)
 		printf("%d", n);
-	}
 	putchar('\n');
 
 	return (0);
