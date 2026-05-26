d1096aad57ff6ad1c02b9a3acf498bb83721ccfd
bumble-beed <shirleen.workstuff@gmail.com>
2026-05-26 10:07:49 +0000

    add file 2

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * main - prints the alphabet, followed by a new line using prototype and putchar
+ *
+ * Return: 0
+ */
+void print_alphabet(void);
+{
+	char lower_case = 'a';
+	
+	while (lower_case <= 'z')
+	{
+		_putchar(c);
+		lower_case++;
+	}
+	_putchar('\n');
+}
+
