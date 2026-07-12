e73f79f41e312e10bb7fa84855b10b4a3c193d1a
bumble-beed <shirleen.workstuff@gmail.com>
2026-07-12 20:31:45 +1000

    Corrections for betty style

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-/*
-* main is entry point
-* 
-* Return = 0
-*/
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-    return (0);
+	return (0);
 }
