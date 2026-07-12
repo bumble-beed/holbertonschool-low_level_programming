f89d8c1b19bee8961b2a0b727f536ad7d259af40
bumble-beed <shirleen.workstuff@gmail.com>
2026-07-12 22:10:11 +1000

    Corrections to code

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	long int li;
 	long long int lli;
 
-	printf("Size of char: %d byte(s)\n", (int)sizeof(c));
-	printf("Size of int: %d bytes(s)\n", (int)sizeof(i));
-	printf("Size of float: %d bytes(s)\n", (int)sizeof(f));
-	printf("Size of long int: %d byte(s)\n", (int)sizeof(li));
-	printf("Size of long long int: %d byte(s)\n", (int)sizeof(lli));
+	printf("Size of a char: %d byte(s)\n", (int)sizeof(c));
+	printf("Size of a int: %d bytes(s)\n", (int)sizeof(i));
+	printf("Size of a float: %d bytes(s)\n", (int)sizeof(f));
+	printf("Size of a long int: %d byte(s)\n", (int)sizeof(li));
+	printf("Size of a long long int: %d byte(s)\n", (int)sizeof(lli));
 
 	return (0);
 }
