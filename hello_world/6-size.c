1a7305c0a62516a522fce0e9337345ef76cfa12a
bumble-beed <shirleen.workstuff@gmail.com>
2026-07-12 22:12:44 +1000

    Corrections to code

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 
 /**
-* main - prints size of various type
-*
-* Return: 0
-*/
+ * main - print size of various types
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	char c;
 	int i;
-	float f;
 	long int li;
 	long long int lli;
+	float f;
 
 	printf("Size of a char: %d byte(s)\n", (int)sizeof(c));
-	printf("Size of an int: %d bytes(s)\n", (int)sizeof(i));
+	printf("Size of an int: %d byte(s)\n", (int)sizeof(i));
 	printf("Size of a long int: %d byte(s)\n", (int)sizeof(li));
 	printf("Size of a long long int: %d byte(s)\n", (int)sizeof(lli));
 	printf("Size of a float: %d byte(s)\n", (int)sizeof(f));
-
 	return (0);
 }
