40f1a00d2df0c05c38d180c5422ff92210f1c865
bumble-beed <shirleen.workstuff@gmail.com>
2026-07-18 23:05:12 +1000

    Print single numbers

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - print single numbers from base 10
+*
+* Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
