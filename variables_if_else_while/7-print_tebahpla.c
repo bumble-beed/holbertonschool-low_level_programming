89c362ece06225548b0c5b7213c213130fe5bed6
bumble-beed <shirleen.workstuff@gmail.com>
2026-07-18 23:14:43 +1000

    Lowercase alphabet in reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - lowercase alphabet in reverse
+*
+* Return: 0
+*/
+int main(void)
+{
+	char ch;
+
+	ch = 'z';
+	while (ch >= 'a')
+		putchar(ch--);
+	putchar('\n');
+
+	return (0);
+}
