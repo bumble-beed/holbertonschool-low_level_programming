3d85dcbd416056c2a5c90fabff57444c8d359a4d
bumble-beed <shirleen.workstuff@gmail.com>
2026-07-19 22:11:19 +1000

    print in lowercase

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - alphabet in lower case
+*
+* Return: void
+*/
+void print_alphabet(void)
+{
+	char lower_case;
+
+	lower_case = 'a';
+	
+	while (lower_case <= 'z')
+	{
+		_putchar(lower_case);
+		lower_case++;
+	}
+
+	_putchar('\n');
+}
