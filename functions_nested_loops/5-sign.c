29dc50ae585492059d6ba97a888fd8700989526f
bumble-beed <shirleen.workstuff@gmail.com>
2026-07-20 21:23:44 +1000

    print sign of number

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,9 +1,8 @@
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 5-main.c 5-sign.c -o 5-sign#include "main.h"
-#include <stdio.h>
+#include "main.h"
 
 /**
 * print_sign - prints sign of number
-@n: number to check
+* @n: number to check
 *
 * Return: 1 if greater than 0, 0 if equal to 0, -1 if less than 0
 */
@@ -16,7 +15,7 @@ int print_sign(int n)
 	}
 	if (n < 0)
 	{
-		_putchar('-1');
+		_putchar('-');
 		return (-1);
 	}
 	else
