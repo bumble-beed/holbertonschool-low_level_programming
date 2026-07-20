551737e8dec927f612ecf9258d880401232a7257
bumble-beed <shirleen.workstuff@gmail.com>
2026-07-20 18:48:55 +1000

    Corrections on loop initialise

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,7 @@
 */
 void print_alphabet_x10(void)
 {
-	int i;	
+	int i;
 	char lower_case;
 
 	i = 0;
@@ -22,7 +22,7 @@ void print_alphabet_x10(void)
 			_putchar(lower_case);
 			lower_case++;
 		}
-	
+
 		_putchar('\n');
 	
 		i++;
