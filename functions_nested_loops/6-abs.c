727640d57bd3844ace478563699e8026d82d9da0
bumble-beed <shirleen.workstuff@gmail.com>
2026-07-20 21:50:53 +1000

    function absolute number

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* abs: print absolute value of interger
+* @n: input number
+*
+* Return: absolute value
+*/
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return(-n);
+	}
+}
