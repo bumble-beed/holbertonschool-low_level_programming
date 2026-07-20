9e6b383087a4c14130bf7856b4a368ef4f34441d
bumble-beed <shirleen.workstuff@gmail.com>
2026-07-20 22:57:39 +1000

    Add 2 numbers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* add - adds two intergers
+* @a: input 1
+* @b: input 2
+
+* Return: Addition result
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
