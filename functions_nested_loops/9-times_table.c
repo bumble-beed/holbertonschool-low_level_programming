f5f7396474a8c3bc6c82d10a6bd7e4f8243af3ca
bumble-beed <shirleen.workstuff@gmail.com>
2026-07-20 22:48:31 +1000

    Times table 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* times_table - print 9 times table
+*
+* Return: no return
+*/
+void times_table(void)
+{
+	int n;
+	int i;
+	int product;
+
+	n = 0;
+	while (n <= 9)
+	{
+		i = 0;
+		while (i <= 9)
+		{
+			product = n * i;
+
+			if (i != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+			if (product > 9)
+			{
+				_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
+			}
+			else
+			{
+				_putchar(product % 10 + '0');
+			}
+			i++;
+		}
+		_putchar('\n');
+		n++;
+	}
+}
