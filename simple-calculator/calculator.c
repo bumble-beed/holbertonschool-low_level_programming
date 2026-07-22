e15c8bb38956f450ca7124ac87d83e298ac4ffc5
bumble-beed <shirleen.workstuff@gmail.com>
2026-07-22 21:25:08 +1000

    Setup skeleton

---
+++ simple-calculator/calculator.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+* main - arithmic calulation value of 2 operends
+*
+* Return: calculated value
+*/
+int main(void)
+{
+	printf("Simple Calculator\n");
+	printf("\n");
+	printf("1) Add\n");
+	printf("2) Subtract\n");
+	printf("3) Multiply\n");
+	printf("4) Divide\n");
+	printf("0) Quit\n");
+	printf("Choice: 0\n");
+	printf("Bye\n");
+
+	if (num1, num2  == '0')
+		printf("Quit\n");
+		break;
+
+return (0);
+}
