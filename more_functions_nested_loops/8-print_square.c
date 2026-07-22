d107d6e2b86b388301deca3217373d3b8a255719
bumble-beed <shirleen.workstuff@gmail.com>
2026-07-22 22:49:02 +1000

    Square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_square - print a square
+* @size: size of square
+*/
+void print_square(int size)
+{
+	int a;
+	int b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+				_putchar('#');
+
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
