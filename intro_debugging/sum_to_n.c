c94292945c1d117f601d20bf254f05a5a3d5a756
bumble-beed <shirleen.workstuff@gmail.com>
2026-08-17 21:36:12 +1000

    Return = sum + n

--- intro_debugging/sum_to_n.c
+++ intro_debugging/sum_to_n.c
@@ -5,10 +5,10 @@ int sum_to_n(int n)
     int i;
     int sum = 0;
 
-    for (i = 1; i < n; i++)
+    for (i = 1; i <= n; i++)
         sum += i;
 
-    return sum + n;
+    return sum;
 }
 
 /* -- DO NOT Modify the code below this line -- */
