792d9a2a727bc25e68f076c6463278633943150b
bumble-beed <shirleen.workstuff@gmail.com>
2026-08-31 21:23:02 +1000

    swap values of int a and b

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* swap_int - function to swap 2 int
+* @a: value of int a
+* @b: value of int b
+*/
+void swap_int(int *a, int *b)
+{
+	int streams;
+
+	streams = a;
+	*a = *b;
+	*b = streams;
+}
