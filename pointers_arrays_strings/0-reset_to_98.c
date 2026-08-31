4b0e8ee1103c17389202ccbf652b004d047a9171
bumble-beed <shirleen.workstuff@gmail.com>
2026-08-31 21:05:15 +1000

    Pointers

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,10 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code 
- *
- * Return: Always 0.
- */
-int main(void)
+* reset_to_98 - function for pointer
+* @n: pointer
+*/
+void reset_to_98(int *n);
 {
-    int n;
-
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
+	*n = 98;
 }
