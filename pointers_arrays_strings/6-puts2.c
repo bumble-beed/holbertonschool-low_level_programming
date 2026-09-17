6c7d1c4e176209b17c7f391264082f49fcb42db8
bumble-beed <shirleen.workstuff@gmail.com>
2026-09-17 22:24:01 +1000

    Changes

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,17 +6,17 @@
 */
 void puts2(char *str)
 {
-    int i;
+int i;
 
-    i = 0;
+i = 0;
 
-    while (str[i] != '\0')
-    {
-        if (i % 2 == 0)
-        {
-            _putchar(str[i]);
-        }
-        i++;
-    }
-    _putchar('\n');
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+{
+_putchar(str[i]);
+}
+i++;
+}
+_putchar('\n');
 }
