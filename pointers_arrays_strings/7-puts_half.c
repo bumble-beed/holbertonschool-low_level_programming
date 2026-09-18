4cdde9f5a154a4a7be9abf8a8090572e5423c855
bumble-beed <shirleen.workstuff@gmail.com>
2026-09-18 15:50:53 +1000

    Changes

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -4,7 +4,6 @@
 * puts_half - print 2nd half of string
 * @str: string
 */
-
 void puts_half(char *str)
 {
 	int len;
@@ -12,17 +11,17 @@ void puts_half(char *str)
 	int i;
 
 	len = 0;
-	
+
 	while (str[len] != '\0')
 	{
 		len++;
 	}
-	start = len / 2; 
-	
+	start = len / 2;
+
 	for (i = start; i < len; i++)
 	{
 		_putchar(str[i]);
 	}
-	
+
 	_putchar('\n');
 }
